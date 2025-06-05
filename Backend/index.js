dbbb44476e0a9159a09cb09624b968da981b901b
Kevin Jaylen <157660471+KJ168@users.noreply.github.com>
2025-06-05 17:18:33 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const routes = require('./routes/route'); 
 
-app.use(cors({
-  origin: "https://mindfulness-three.vercel.app", 
-  credentials: true
-}));
+// Buka akses ke semua origin
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Untuk menangani preflight request
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use('/api', routes);

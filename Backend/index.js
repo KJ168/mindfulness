1de5c18a91d6bc38d124721b5158415be9849039
Kevin Jaylen <157660471+KJ168@users.noreply.github.com>
2025-06-05 17:42:03 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/route'); 
 
-// Buka akses ke semua origin
+// Middleware untuk mengizinkan semua origin (tanpa cors package)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*'); // Semua domain boleh akses
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  // Untuk menangani preflight request
   if (req.method === 'OPTIONS') {
+    // Untuk preflight requests
     return res.sendStatus(200);
   }
   next();
